86e022bc8d292e5746053a24f0a39a244b3e32e4
vighneshh <gawadvighnesh@gmail.com>
2019-08-19 16:05:05 -0800

    Inital Commit

--- url_shortener/forms.py
+++ url_shortener/forms.py
@@ -1,5 +1,5 @@
 from  django import forms
 
 class UrlForm(forms.Form):
-    MainUrl = forms.CharField()
-    CustomUrl = forms.CharField(required=False)
+    MainUrl = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter url you want to short'}))
+    CustomUrl = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter name you want as suffix to shorturl'}),required=False)
--- url_shortener/views.py
+++ url_shortener/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render,redirect
+from django.contrib import messages
 from .models import Url
 from .forms import UrlForm
+import string
+import random
 
 def RedirectShorturl(request, shortname):
     try:
@@ -14,8 +17,24 @@ def GetUrls(request):
     if request.method == 'POST':
         form = UrlForm(request.POST)
         if form.is_valid():
-            customurl = form.cleaned_data['CustomUrl']
-            print(customurl)
+            shortname = form.cleaned_data['CustomUrl']
+            if shortname == '':
+                shortname = generator()
+                url = Url(url=form.cleaned_data['MainUrl'],shortname=shortname)
+                url.save()
+                return render(request,'url_shortener/shorturl_created.html')
+            else:
+                try:
+                    url = Url(url=form.cleaned_data['MainUrl'],shortname=shortname)
+                    url.save()
+                    return render(request,'url_shortener/shorturl_created.html')
+                except:
+                    messages.info(request,'Url is already taken. Try with different name.')
+                    return redirect('home')
+
     form = UrlForm()
     return render(request,'url_shortener/url_short.html',{'form':form})
+
+def generator(size=8, chars=string.ascii_lowercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
 
